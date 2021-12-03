5f969b795526d88f509738c80e726a8d19b3c298
Lesley159 <lesleyhope159@gmail.com>
2021-12-03 13:46:42 +0300

    add first commit

---
+++ js/script.js
@@ -0,0 +1,41 @@
+
+$(document).ready(function(){
+    $(".col-md-3").hover(function(){
+      $("#ontario").toggleClass("hovereffect");
+  
+    });
+  });
+    $(".col-md-3").hover(function(){
+      $("#studio").toggleClass("hovereffect");
+  
+    });
+      $(".col-md-3").hover(function(){
+        $("#jim").toggleClass("hovereffect");
+  
+      });
+        $(".col-md-3").hover(function(){
+          $("#day").toggleClass("hovereffect");
+    });
+      $(".col-md-3").hover(function(){
+      $("#calculator").toggleClass("hovereffect");
+  
+      });
+      $(".col-md-3").hover(function(){
+      $("#burned").toggleClass("hovereffect");
+  
+      });
+    $(document).ready(function() {
+    $(".design").click(function() {
+      $(".image3").slideToggle();
+      $(".de").slideToggle();
+    });
+  });
+    $(".develop").click(function() {
+      $(".image1").slideToggle();
+      $(".dev").slideToggle();
+    });
+  
+    $(".product").click(function() {
+      $(".image2").slideToggle();
+      $(".pro").fadeToggle();
+    });
